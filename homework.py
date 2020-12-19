35325ef68198b454f10b6fe69f00d6b2e8375daf
Дмитрий <Fable1905@yandex.ru>
2020-12-19 21:50:34 +0300

    Внес поправки

--- homework.py
+++ homework.py
@@ -21,69 +21,59 @@ class Calculator:
         self.records.append(Record)
 
     def get_today_stats(self):
-        num = 0
         moment = dt.datetime.now().date()
-        for i in self.records:
-            if i.date == moment:
-                num += i.amount
+        num = sum([i.amount for i in self.records if i.date == moment])
         return num
 
     def get_week_stats(self):
-        number = 0
-        for i in self.records:
-            today = dt.datetime.now().date()
-            week = today - dt.timedelta(days=7)
-            if week <= i.date <= today:
-                number += i.amount
+        today = dt.datetime.now().date()
+        week = today - dt.timedelta(days=7)
+        number = sum([i.amount for i in self.records if week <= i.date <= today])
         return number
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        s = self.get_today_stats()
-        if s < self.limit:
-            other = self.limit - s
+        number = self.get_today_stats()
+        if number < self.limit:
+            other = self.limit - number
             return ('Сегодня можно съесть что-нибудь ещё, '
                     f'но с общей калорийностью не более {round(other, 2)} кКал')
-        elif s >= self.limit:
+        elif number >= self.limit:
             return ('Хватит есть!')
 
 
 class CashCalculator(Calculator):
-    USD_RATE = 75.12
-    EURO_RATE = 85.31
+    USD_RATE = 73.12
+    EURO_RATE = 85.12
+
     def get_today_cash_remained(self, currency):
         today_stats = self.get_today_stats()
         other_limit = self.limit - today_stats
-        if currency == 'usd':
-            rate = other_limit / self.USD_RATE
-            forex = 'USD'
-        elif currency == 'eur':
-            rate = other_limit / self.EURO_RATE
-            forex = 'Euro'
-        elif currency == 'rub':
-            rate = other_limit
-            forex = 'руб'
-        else:
-            return ('не найдено')
+        self.rate = {
+             'usd': (self.USD_RATE, 'USD'),
+             'eur': (self.EURO_RATE, 'Euro'),
+             'rub': (1, 'руб')
+        }
+
+        for i in self.rate.keys():
+            if i == currency:
+                currency_number = self.rate[i][0]
+                currency_name = self.rate[i][1]
         if today_stats < self.limit:
-            return (f'На сегодня осталось {round(rate, 2)} {forex}')
+            return (f'На сегодня осталось {round(other_limit / currency_number, 2)} {currency_name}')
         elif today_stats == self.limit:
             return ('Денег нет, держись')
         else:
-            q = abs(rate)
-            return (f'Денег нет, держись: твой долг - {round(q, 2)} {forex}')
+            debt = abs(other_limit)
+            return (f'Денег нет, держись: твой долг - {round(debt / currency_number, 2)} {currency_name}')
+
 
 
 cash_calculator = CashCalculator(1000)
 
-cash_calculator.add_record(Record(amount=145, comment="кофе"))
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-print(cash_calculator.get_today_cash_remained("rub"))
-r1 = Record(amount=145, comment="Безудержный шопинг", date="08.03.2019")
-r2 = Record(amount=1568, comment="Наполнение потребительской корзины", date="09.03.2019")
-r3 = Record(amount=691, comment="Катание на такси")
-print(r1.date)
-print(r2.date)
-print(r3.date)
+if __name__ == '__main__':
+    cash_calculator.add_record(Record(amount=145, comment="кофе"))
+    cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
+    cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
+    print(cash_calculator.get_today_cash_remained("usd"))
